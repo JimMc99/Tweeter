4f0d6eb02afafb619fcb42b33da82adf541eb07a
JimMc99 <31132594+JimMc99@users.noreply.github.com>
2019-11-12 14:22:53 +0000

    added a model_order to use ngrams to search for successive words

--- tweeter.py
+++ tweeter.py
@@ -13,49 +13,56 @@ def list_lines(fpath):
 
 
 class Tweeter:
-    def __init__(self, fpath):
+    def __init__(self, fpath, model_order=2):
         self.fpath = fpath
 
-        self.markov_model_fo = defaultdict(lambda: Counter())
-        self.start_words = Counter()
+        self.model_order = model_order  # length of ngrams the model looks at to find next word
+        self.markov_model = defaultdict(lambda: Counter())
+        self.start_ngrams = Counter()
         self.__load_model()
 
     def __load_model(self):
         # load model (prepare data)
-        # first-order markov model of words (no preprocessing)
-        # each token key goes to a Counter value containing counts of the subsequent word
-        # to add to this model, markov_model_fo[current_word][next_word] += 1
+        # nth order (depends on self.model_order) markov model of words (no preprocessing)
+        # to add to this model, markov_model[current_ngram][next_word] += 1
         tweets = list_lines(self.fpath)
 
         for tweet in tweets:
             tokens = tweet.split()
 
-            # add each token except the last token in the tweet to the markov model
-            for i, token in enumerate(tokens[:-1]):
-                self.markov_model_fo[token][tokens[i + 1]] += 1
-            # for the last token, nothing follows
-            if len(tokens):
-                self.markov_model_fo[tokens[-1]][None] += 1
-                self.start_words[tokens[0]] += 1
+            # add each ngram except the last ngram in the tweet to the markov model
+            for i in range(len(tokens[:-self.model_order])):
+                current_ngram = tuple(tokens[i:i+self.model_order])
+                next_word = tokens[i + self.model_order]
+                self.markov_model[current_ngram][next_word] += 1
+            # for the last ngram, nothing follows
+            if len(tokens) >= self.model_order:
+                current_ngram = tuple(tokens[-self.model_order:])
+                next_word = None
+                self.markov_model[current_ngram][next_word] += 1
+
+                start_ngram = tuple(tokens[:self.model_order])
+                self.start_ngrams[start_ngram] += 1
 
     def new_tweet(self):
         # create new tweet from model
         new_tweet_list = []
 
-        start_keys = list(self.start_words.keys())
-        start_values = list(self.start_words.values())
-        start_word = random.choices(start_keys, weights=start_values)[0]
+        start_ngrams = list(self.start_ngrams.keys())
+        start_values = list(self.start_ngrams.values())
+        start_ngram = random.choices(start_ngrams, weights=start_values)[0]
 
-        current_word = start_word
+        current_ngram = start_ngram
 
-        while current_word is not None:
-            new_tweet_list.append(current_word)
+        while None not in current_ngram:
+            new_tweet_list.append(current_ngram[0])
 
-            next_keys = list(self.markov_model_fo[current_word].keys())
-            next_values = list(self.markov_model_fo[current_word].values())
-            next_word = random.choices(next_keys, weights=next_values)[0]
+            next_words = list(self.markov_model[current_ngram].keys())
+            next_values = list(self.markov_model[current_ngram].values())
+            next_word = random.choices(next_words, weights=next_values)[0]
 
-            current_word = next_word
+            current_ngram = tuple([current_ngram[1], next_word])
+        new_tweet_list.extend(current_ngram[:-1])
 
         return ' '.join(new_tweet_list)
 
